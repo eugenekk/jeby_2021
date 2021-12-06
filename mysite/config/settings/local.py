02a65cbaa2ab40274944075ae35eace7e9f946f3
eugenekk <08youjin12@gmail.com>
2021-12-06 15:42:07 +0900

    local 환경 준비 완료

---
+++ mysite/config/settings/local.py
@@ -0,0 +1,5 @@
+# Local에서만 사용하는 설정 파일
+
+from .base import *
+
+DEBUG = True
---
+++ mysite/config/settings/prod.py
@@ -0,0 +1,10 @@
+# 상용환경용 설정파일
+from .base import *
+
+
+DEBUG = False
+
+
+STATIC_ROOT = BASE_DIR / "static/"
+
+STATICFILES_DIRS = []
