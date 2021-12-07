45a53d356c2ab06c7d5435e653982f83d94a9bd9
eugenekk <08youjin12@gmail.com>
2021-12-07 14:21:22 +0900

    news앱 생성

--- mysite/config/settings/base.py
+++ mysite/config/settings/base.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "common",
+    "news",
 ]
 
 MIDDLEWARE = [
@@ -143,3 +144,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
